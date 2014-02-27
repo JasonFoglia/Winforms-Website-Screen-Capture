ec7b2c1628d6ad8d1f52c8d185f8e61aac2d56ec
Jason Foglia <jason.fogia@gmail.com>
2014-02-26 19:34:57 -0700

    First commit

---
+++ Backup/ScreenshotCapturer/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class ScreenShot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        private static System.Uri url = new System.Uri("https://admin.escoserver.com");
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.urlTb = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.featuresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.takeScreenShotToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.alertToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.backToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.forwardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loginContainer = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.login = new System.Windows.Forms.Button();
+            this.PassWord = new System.Windows.Forms.TextBox();
+            this.UserName = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.loginContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(0, 53);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(950, 721);
+            this.webBrowser.TabIndex = 0;
+            this.webBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser_DocumentCompleted);
+            // 
+            // urlTb
+            // 
+            this.urlTb.Location = new System.Drawing.Point(6, 29);
+            this.urlTb.Name = "urlTb";
+            this.urlTb.Size = new System.Drawing.Size(877, 20);
+            this.urlTb.TabIndex = 3;
+            this.urlTb.Text = "http://";
+            this.urlTb.KeyUp += new System.Windows.Forms.KeyEventHandler(this.urlTb_KeyUp);
+            // 
+            // button1
+            // 
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(889, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Go_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "\"JPG (*.jpg)|*.jpg\"";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.featuresToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.backToolStripMenuItem,
+            this.forwardToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Margin = new System.Windows.Forms.Padding(100, 0, 0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(951, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.printToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // printToolStripMenuItem
+            // 
+            this.printToolStripMenuItem.Enabled = false;
+            this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(99, 22);
+            this.printToolStripMenuItem.Text = "Print";
+            this.printToolStripMenuItem.Click += new System.EventHandler(this.printToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(99, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // featuresToolStripMenuItem
+            // 
+            this.featuresToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.takeScreenShotToolStripMenuItem,
+            this.alertToolStripMenuItem});
+            this.featuresToolStripMenuItem.Name = "featuresToolStripMenuItem";
+            this.featuresToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.featuresToolStripMenuItem.Text = "Features";
+            // 
+            // takeScreenShotToolStripMenuItem
+            // 
+            this.takeScreenShotToolStripMenuItem.Name = "takeScreenShotToolStripMenuItem";
+            this.takeScreenShotToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.takeScreenShotToolStripMenuItem.Text = "Take Screen Shot";
+            this.takeScreenShotToolStripMenuItem.Click += new System.EventHandler(this.takeScreenShotToolStripMenuItem_Click);
+            // 
+            // alertToolStripMenuItem
+            // 
+            this.alertToolStripMenuItem.Enabled = false;
+            this.alertToolStripMenuItem.Name = "alertToolStripMenuItem";
+            this.alertToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.alertToolStripMenuItem.Text = "Alert";
+            this.alertToolStripMenuItem.Click += new System.EventHandler(this.alertToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.toolStripMenuItem1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripMenuItem1.Enabled = false;
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(22, 20);
+            this.toolStripMenuItem1.Text = "|";
+            // 
+            // backToolStripMenuItem
+            // 
+            this.backToolStripMenuItem.Name = "backToolStripMenuItem";
+            this.backToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.backToolStripMenuItem.Text = "Back";
+            this.backToolStripMenuItem.Click += new System.EventHandler(this.backToolStripMenuItem_Click);
+            // 
+            // forwardToolStripMenuItem
+            // 
+            this.forwardToolStripMenuItem.Name = "forwardToolStripMenuItem";
+            this.forwardToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.forwardToolStripMenuItem.Text = "Forward";
+            this.forwardToolStripMenuItem.Click += new System.EventHandler(this.forwardToolStripMenuItem_Click);
+            // 
+            // loginContainer
+            // 
+            this.loginContainer.Controls.Add(this.label2);
+            this.loginContainer.Controls.Add(this.label1);
+            this.loginContainer.Controls.Add(this.login);
+            this.loginContainer.Controls.Add(this.PassWord);
+            this.loginContainer.Controls.Add(this.UserName);
+            this.loginContainer.Location = new System.Drawing.Point(0, 0);
+            this.loginContainer.Name = "loginContainer";
+            this.loginContainer.Size = new System.Drawing.Size(951, 774);
+            this.loginContainer.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(403, 357);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(396, 316);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "User Name";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // login
+            // 
+            this.login.Location = new System.Drawing.Point(433, 405);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(75, 23);
+            this.login.TabIndex = 2;
+            this.login.Text = "Login";
+            this.login.UseVisualStyleBackColor = true;
+            this.login.Click += new System.EventHandler(this.login_Click);
+            // 
+            // PassWord
+            // 
+            this.PassWord.Location = new System.Drawing.Point(469, 354);
+            this.PassWord.Name = "PassWord";
+            this.PassWord.Size = new System.Drawing.Size(100, 20);
+            this.PassWord.TabIndex = 1;
+            this.PassWord.UseSystemPasswordChar = true;
+            // 
+            // UserName
+            // 
+            this.UserName.Location = new System.Drawing.Point(469, 313);
+            this.UserName.Name = "UserName";
+            this.UserName.Size = new System.Drawing.Size(100, 20);
+            this.UserName.TabIndex = 0;
+            // 
+            // ScreenShot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(951, 774);
+            this.Controls.Add(this.loginContainer);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.urlTb);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ScreenShot";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Screen Shot";
+            this.Load += new System.EventHandler(this.ScreenShot_Load);
+            this.Resize += new System.EventHandler(this.ScreenShot_Resize);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.loginContainer.ResumeLayout(false);
+            this.loginContainer.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.TextBox urlTb;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem featuresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem takeScreenShotToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem alertToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem forwardToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.Panel loginContainer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button login;
+        private System.Windows.Forms.TextBox PassWord;
+        private System.Windows.Forms.TextBox UserName;
+
+    }
+}
+
---
+++ ScreenshotCapturer/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace WindowsFormsApplication1
+{
+    public partial class ScreenShot : Form
+    {
+        private WebBrowser wb;
+        private Bitmap jpg;
+
+        public ScreenShot()
+        {
+            InitializeComponent();
+        }
+
+        private void ScreenShot_Load(object sender, EventArgs e)
+        {
+            ActiveControl = UserName;
+        }
+
+        private void EnableStuff()
+        {
+            button1.Enabled = true;
+            printToolStripMenuItem.Enabled = true;
+            alertToolStripMenuItem.Enabled = true;
+        }
+
+        private void DisableStuff()
+        {
+            button1.Enabled = false;
+            printToolStripMenuItem.Enabled = false;
+            alertToolStripMenuItem.Enabled = false;
+        }
+
+        private void webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            wb = sender as WebBrowser;
+            urlTb.Text = wb.Url.AbsoluteUri;
+            EnableStuff();
+        }
+
+        private void Go_Click(object sender, EventArgs e)
+        {
+            string address = "";
+            if (!urlTb.Text.ToString().StartsWith("http://") && !urlTb.Text.ToString().StartsWith("https://"))
+            {
+                address = "http://" + urlTb.Text.ToString();
+            }
+            else
+            {
+                address = urlTb.Text.ToString();
+            }
+
+            DisableStuff();
+            wb.Url = new Uri(address);
+            wb.DocumentCompleted -= new WebBrowserDocumentCompletedEventHandler(this.webBrowser_DocumentCompleted);
+            wb.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(this.webBrowser_DocumentCompleted);
+        }
+
+        private void urlTb_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.ToString() == "Return")
+            {
+                DisableStuff();
+                wb.Url = new Uri(urlTb.Text.ToString());
+                wb.DocumentCompleted -= new WebBrowserDocumentCompletedEventHandler(this.webBrowser_DocumentCompleted);
+                wb.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(this.webBrowser_DocumentCompleted);
+            }
+        }
+        
+        public System.Drawing.Bitmap CaptureWebPage(string URL)
+        {
+            WebBrowser web = new WebBrowser();
+            web.ScrollBarsEnabled = false;
+            web.ScriptErrorsSuppressed = true;
+            web.Navigate(URL);
+
+            while (web.ReadyState != WebBrowserReadyState.Complete)
+                Application.DoEvents();
+            Thread.Sleep(1500);
+
+            web.Width = wb.Document.Body.ScrollRectangle.Width + 50;
+            web.Height = wb.Document.Body.ScrollRectangle.Height + 50;
+
+            Bitmap bmp = new Bitmap(web.Width, web.Height);
+            web.DrawToBitmap(bmp, new Rectangle(0, 0, web.Width, web.Height));
+
+            return bmp;
+        }
+
+        private void ScreenShot_Resize(object sender, EventArgs e)
+        {
+            if (webBrowser != null)
+            {
+                webBrowser.Width = this.Width - 19;
+                webBrowser.Height = this.Height - (webBrowser.Location.Y * 2) + 18;
+            }
+
+            if (urlTb != null)
+            {
+                urlTb.Width = this.Width - urlTb.Location.X - button1.Width - 26;
+                button1.SetBounds(urlTb.Width + urlTb.Location.X + 5, button1.Location.Y, button1.Width, button1.Height);
+            }
+        }
+
+
+        private void takeScreenShotToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (wb != null)
+            {
+                jpg = CaptureWebPage(wb.Url.AbsoluteUri.ToString());
+
+                saveFileDialog1.ShowDialog();
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    if (saveFileDialog1.FileName != "")
+                    {
+                        jpg.Save(saveFileDialog1.FileName, ImageFormat.Jpeg);
+                        jpg = null;
+                    }                    
+                }                
+            } 
+        }
+
+        private void alertToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < wb.Document.Forms.Count; i++)
+            {
+                foreach (HtmlElement g in wb.Document.Forms[i].Children)
+                {
+                    HtmlElement forms = g;
+                    for (int t = 0; t < forms.GetElementsByTagName("input").Count; t++)
+                    {
+                        wb.Document.Window.Alert(forms.GetElementsByTagName("input")[t].GetAttribute("type").ToString() + " | " + forms.GetElementsByTagName("input")[t].GetAttribute("name").ToString());//forms.GetElementsByTagName("input")[t].GetAttribute("value").ToString()
+                    }
+                    if (forms.TagName == "INPUT")
+                    {
+                        //wb.Document.Window.Alert(forms.GetAttribute("value") +" | "+ forms.InnerText);
+                    }
+                }
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void printToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            wb.ShowPrintDialog();
+        }
+
+        private void backToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoBack();
+        }
+
+        private void forwardToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoForward();
+        }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            loginContainer.Visible = false;
+            //webBrowser.Navigate(url.AbsoluteUri + "?user[]=JAYAF&pass[]=test");
+            byte[] d = ASCIIEncoding.ASCII.GetBytes("jay=true&user=" + UserName.Text + "&pass=" + PassWord.Text);
+            webBrowser.Navigate(url.AbsoluteUri, "", d, "Content-Type: application/x-www-form-urlencoded" + "\n" + "\r");
+        }
+    }
+}
